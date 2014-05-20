17a1b6c190797a8920e0493a83f3d984b819fc4c
Guilherme_Kenji Chihaya <chihayakenji1893@gmail.com>
2014-05-20 17:05:35 +0200

    Namespace exports new functions Function for finding the name of the group with largest number in an area

--- R/entropy_diversity.R
+++ R/entropy_diversity.R
@@ -1,15 +1,14 @@
 entropy_diversity <- function(data,...){
   #define internal variables
-  popsize <- rowSums(data[,unlist(list(...))])
+  pop <- rowSums(data[,unlist(list(...))])
                      
   #calculate the proportions for each group in the area
-  p_frame <- data.frame(lapply(data[,unlist(list(...))],function(x){x/popsize}))
-                                                                                            
+  p_i <- data.frame(lapply(data[,unlist(list(...))],function(x){x/pop}))
+
   #calculate entropy
-  entropy <- -1*((1/log(popsize))*rowSums(data.frame(lapply(p_frame,
-                                                            function(x){x*log(x)})
-                                                     )
-                                          )
-                 )
-  return(entropy)
+  h <- -1*((1/log(pop))*rowSums(data.frame(lapply(p_i,
+                                                  function(x){
+                                                              x*log(x)
+                                                              }))))
+  return(h)
 }
