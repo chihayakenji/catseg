04d2e110befce36a129f69766e86eacb7873db19
chihayakenji <chihayakenji1893@gmail.com>
2014-05-21 07:10:48 +0200

    wsm.r provisory version (returns strings)

--- R/wsm.r
+++ R/wsm.r
@@ -1,17 +1,27 @@
 #based on:
 #Wright, R., Holloway, S., & Ellis, M. (2011). Reconsidering both Diversity and Segregation: A Reply to Poulsen, Johnston and Forrest, and to Peach. Journal of Ethnic and Migration Studies, 37(1), 167–176. doi:10.1080/1369183X.2011.523865
 
-wsm <- function(data,unitid,unitpop,...){
+wsm <- function(data,...){
   #calculate the entropy index
-  diversity <- entropy_diversity(data,unitid,unitpop,..)
+  diversity <- entropy_diversity(data,...)
   
   #find out what is the most numerous group
+  domin <- largest_group_name(data,...)
   
-  domin <- largest_group(data,...)
+  #find out the size of the largest group
+  l_size <- largest_group(data,...)
+  
   #classify
-  #low diversity tract
-    #entropy <= 0.3707 or one group > .80 of pop
-  #high diversity tract
-    #entropy >= 0.7414 and no group > .45 of pop
   #moderately diverse: all others
+  result <- "moderate diversity"
+  #low diversity tract
+  result <- ifelse(diversity <= 0.3707, "low diversity",result)
+  result <- ifelse(l_size > .80, "low diversity", result)
+  #high diversity tract
+  result <- ifelse(diversity >= 0.7414 & l_size <= .45, "high diversity", result)
+  #find out what is the most numerous group
+  domin <- largest_group_name(data,...)
+  result <- paste(result,domin,"neighbourhood",sep=" ")
+  
+  
 }
