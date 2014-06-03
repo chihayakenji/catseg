c2cbaca21a933a22f15037db74d4a407e83ad50d
chihayakenji <chihayakenji1893@gmail.com>
2014-06-03 14:07:11 +0200

    Fixed bug that made entropy be compared to total pop instead of proportion of pop.

--- R/whe.r
+++ R/whe.r
@@ -1,6 +1,6 @@
 #based on:
 #Wright, R., Holloway, S., & Ellis, M. (2011). Reconsidering both Diversity and Segregation: A Reply to Poulsen, Johnston and Forrest, and to Peach. Journal of Ethnic and Migration Studies, 37(1), 167–176. doi:10.1080/1369183X.2011.523865
-
+#'@export
 whe <- function(data,...){
   #calculate the entropy index
   diversity <- entropy_diversity(data,...)
@@ -11,14 +11,16 @@ whe <- function(data,...){
   #find out the size of the largest group
   l_size <- largest_group(data,...)
   
+  #find the size of the area population
+  pop <- rowSums(data[,unlist(list(...))])
   #classify
   #moderately diverse: all others
   result <- "moderate diversity"
   #low diversity tract
   result <- ifelse(diversity <= 0.3707, "low diversity",result)
-  result <- ifelse(l_size > .8, "low diversity", result)
+  result <- ifelse(l_size/pop > .8, "low diversity", result)
   #high diversity tract
-  result <- ifelse(diversity >= 0.7414 & l_size <= .45, "high diversity", result)
+  result <- ifelse(diversity >= 0.7414 & l_size/pop <= .45, "high diversity", result)
   #find out what is the most numerous group
   domin <- largest_group_name(data,...)
   result <- paste(result,domin,"neighbourhood",sep=" ")
